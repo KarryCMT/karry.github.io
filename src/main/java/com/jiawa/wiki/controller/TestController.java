e7b7fc7c33eedf5f25b0dd4504c9c02ef3eda35e
karry_cmt <1570606465@qq.com>
2021-11-21 10:55:30 +0800

    feat: 增加了自定义配置项的示例

--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,14 +1,20 @@
 package com.jiawa.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
+//    获取配置项里面的配置
+//  @Value("${test.hello:Test}") 默认配置
+    @Value("${test.hello}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello(){
-        return "hello World";
+        return "hello World！"+testHello;
     }
 
     @PostMapping("/hello/post")
