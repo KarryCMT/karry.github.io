8b6847829e22ccf6fff735ae0d1bbabb248c3910
karry_cmt <1570606465@qq.com>
2021-11-21 14:10:32 +0800

    fix: mybatis使用实例，开发一个test/list接口，查询所有的数据

--- src/main/java/com/jiawa/wiki/config/WikiApplication.java
+++ src/main/java/com/jiawa/wiki/config/WikiApplication.java
@@ -1,5 +1,6 @@
 package com.jiawa.wiki.config;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.env.Environment;
 //@ComponentScan({"com.jiawa"}) 扫描多个包
 @ComponentScan("com.jiawa") //扫描单个包
 @SpringBootApplication
+@MapperScan("com.jiawa.wiki.mapper")
 public class WikiApplication {
 
 	private static final Logger LOG = LoggerFactory.getLogger(WikiApplication.class);
--- src/main/java/com/jiawa/wiki/controller/TestController.java
+++ src/main/java/com/jiawa/wiki/controller/TestController.java
@@ -1,9 +1,14 @@
 package com.jiawa.wiki.controller;
 
+import com.jiawa.wiki.domain.Test;
+import com.jiawa.wiki.service.TestService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -11,6 +16,8 @@ public class TestController {
 //  @Value("${test.hello:Test}") 默认配置
     @Value("${test.hello}")
     private String testHello;
+    @Resource
+    private TestService testService;
 
     @GetMapping("/hello")
     public String hello(){
@@ -21,4 +28,11 @@ public class TestController {
     public String helloPost(String name){
         return "hello World Post " + name;
     }
+    // 查询出来返回一个数组对象
+    @GetMapping("/test/list")
+    public List<Test> list(){
+        return testService.list();
+    }
+
+
 }
