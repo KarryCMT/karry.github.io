88b043c96dce658a86c64a3c14c17de3f50b922d
karry_cmt <1570606465@qq.com>
2021-11-21 16:15:14 +0800

    fix: 封装请求参数和返回参数

--- src/main/java/com/jiawa/wiki/controller/EbookController.java
+++ src/main/java/com/jiawa/wiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.jiawa.wiki.controller;
 
-import com.jiawa.wiki.domain.Ebook;
+import com.jiawa.wiki.req.EbookReq;
+import com.jiawa.wiki.req.EbookResp;
 import com.jiawa.wiki.resp.CommonResp;
 import com.jiawa.wiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +20,9 @@ public class EbookController {
 
     // 查询出来返回一个数组对象
     @GetMapping("/list")
-    public CommonResp list(){
-        CommonResp<List<Ebook>> resp = new CommonResp<>();
-        List<Ebook> list = ebookService.list();
+    public CommonResp list(EbookReq req){
+        CommonResp<List<EbookResp>> resp = new CommonResp<>();
+        List<EbookResp> list = ebookService.list(req);
         resp.setContent(list);
         return resp;
     }
