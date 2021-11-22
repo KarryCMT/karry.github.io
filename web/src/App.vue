af3f2795a7ab6390a1a0e86d0d1c62c10f898caa
karry_cmt <1570606465@qq.com>
2021-11-22 21:11:40 +0800

    fix: 新增the-footer组件

--- web/src/App.vue
+++ web/src/App.vue
@@ -3,19 +3,19 @@
     <the-header />
     <a-layout>
         <router-view />
-        <a-layout-footer style="text-align: center">
-          甲娃电子书
-        </a-layout-footer>
+        <the-footer />
       </a-layout>
   </a-layout>
 </template>
 <script lang="ts">
 import { defineComponent } from 'vue';
 import TheHeader from '@/components/the-header.vue';
+import TheFooter from '@/components/the-footer.vue';
 export default defineComponent({
   name: "app",
   components:{
-    TheHeader
+    TheHeader,
+    TheFooter
   }
 })
 </script>
