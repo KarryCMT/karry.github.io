9ba6ee8633b96e06fcd32b97a92f9046cb08a5f0
karry_cmt <1570606465@qq.com>
2021-11-25 21:32:56 +0800

    fix: 新增文档

---
+++ src/main/java/com/jiawa/wiki/service/DocService.java
@@ -0,0 +1,72 @@
+package com.jiawa.wiki.service;
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.jiawa.wiki.domain.Doc;
+import com.jiawa.wiki.domain.DocExample;
+import com.jiawa.wiki.mapper.DocMapper;
+import com.jiawa.wiki.req.DocQueryReq;
+import com.jiawa.wiki.req.DocSaveReq;
+import com.jiawa.wiki.resp.DocQueryResp;
+import com.jiawa.wiki.resp.PageResp;
+import com.jiawa.wiki.util.CopyUtil;
+import com.jiawa.wiki.util.SnowFlake;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@Service
+public class DocService {
+    @Resource
+    private DocMapper docMapper;
+    @Resource
+    private SnowFlake snowFlake;
+
+    public PageResp<DocQueryResp> list(DocQueryReq req) {
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        DocExample.Criteria criteria = docExample.createCriteria();
+        // 列表复制
+        PageHelper.startPage(req.getPage(), req.getSize());
+        List<Doc> docList = docMapper.selectByExample(docExample);
+        PageInfo<Doc> pageInfo = new PageInfo<>(docList);
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        PageResp<DocQueryResp> pageResp = new PageResp();
+        pageResp.setTotal(pageInfo.getTotal());
+        pageResp.setList(list);
+        return pageResp;
+    }
+
+    public List<DocQueryResp> all() {
+        DocExample docExample = new DocExample();
+        docExample.setOrderByClause("sort asc");
+        List<Doc> docList = docMapper.selectByExample(docExample);
+
+        List<DocQueryResp> list = CopyUtil.copyList(docList, DocQueryResp.class);
+
+        return list;
+    }
+
+    public void save(DocSaveReq req) {
+        Doc doc = CopyUtil.copy(req, Doc.class);
+        if (ObjectUtils.isEmpty(req.getId())) {
+            // 新增
+            // 生成雪花id
+            doc.setId(snowFlake.nextId());
+            docMapper.insert(doc);
+        } else {
+            // 修改
+            docMapper.updateByPrimaryKey(doc);
+
+        }
+    }
+
+    public void delete(Long id) {
+        docMapper.deleteByPrimaryKey(id);
+    }
+
+
+}
