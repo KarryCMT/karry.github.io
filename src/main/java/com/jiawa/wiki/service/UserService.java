f513632d03d3e57d4a933b11b2d6e635598b354f
karry_cmt <1570606465@qq.com>
2021-11-27 11:50:05 +0800

    fix: 增加Redis

--- src/main/java/com/jiawa/wiki/service/UserService.java
+++ src/main/java/com/jiawa/wiki/service/UserService.java
@@ -4,14 +4,20 @@ import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import com.jiawa.wiki.domain.User;
 import com.jiawa.wiki.domain.UserExample;
+import com.jiawa.wiki.exception.BusinessException;
+import com.jiawa.wiki.exception.BusinessExceptionCode;
 import com.jiawa.wiki.mapper.UserMapper;
+import com.jiawa.wiki.req.UserLoginReq;
 import com.jiawa.wiki.req.UserQueryReq;
+import com.jiawa.wiki.req.UserResetPasswordReq;
 import com.jiawa.wiki.req.UserSaveReq;
-import com.jiawa.wiki.resp.UserQueryResp;
 import com.jiawa.wiki.resp.PageResp;
+import com.jiawa.wiki.resp.UserLoginResp;
+import com.jiawa.wiki.resp.UserQueryResp;
 import com.jiawa.wiki.util.CopyUtil;
 import com.jiawa.wiki.util.SnowFlake;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
@@ -28,7 +34,7 @@ public class UserService {
         UserExample userExample = new UserExample();
         UserExample.Criteria criteria = userExample.createCriteria();
         if (!ObjectUtils.isEmpty(req.getLoginName())) {
-            criteria.andNameLike("%" + req.getLoginName() + "%");
+            criteria.andLoginNameEqualTo(req.getLoginName());
         }
 
 //        列表复制
@@ -46,19 +52,73 @@ public class UserService {
     public void save(UserSaveReq req) {
         User user = CopyUtil.copy(req, User.class);
         if (ObjectUtils.isEmpty(req.getId())) {
-            // 新增
-            // 生成雪花id
-            user.setId(snowFlake.nextId());
-            userMapper.insert(user);
+            User userDB = selectByLoginName(req.getLoginName());
+            if (ObjectUtils.isEmpty(userDB)) {
+                // 新增
+                // 生成雪花id
+                user.setId(snowFlake.nextId());
+                userMapper.insert(user);
+            } else {
+                throw new BusinessException(BusinessExceptionCode.USER_LOGIN_NAME_EXIST);
+            }
+
         } else {
             // 修改
-            userMapper.updateByPrimaryKey(user);
+            user.setLoginName(null);
+            user.setPassword(null);
+            userMapper.updateByPrimaryKeySelective(user);
 
         }
     }
 
-    public void delete(Long id){
+    public void delete(Long id) {
         userMapper.deleteByPrimaryKey(id);
+    }
+
+    public User selectByLoginName(String LoginName) {
+        UserExample userExample = new UserExample();
+        UserExample.Criteria criteria = userExample.createCriteria();
+
+        criteria.andLoginNameEqualTo(LoginName);
+
+
+        List<User> userList = userMapper.selectByExample(userExample);
+        if (CollectionUtils.isEmpty(userList)) {
+            return null;
+        } else {
+            return userList.get(0);
+        }
+
+    }
+
+    public void resetPassword(UserResetPasswordReq req) {
+        User user = CopyUtil.copy(req, User.class);
+        userMapper.updateByPrimaryKeySelective(user);
+    }
+
+    public UserLoginResp login(UserLoginReq req) {
+        User userDB = selectByLoginName(req.getLoginName());
+        System.out.println(userDB);
+        if (ObjectUtils.isEmpty(userDB)) {
+//            LOG.info("用户名不存在，{}",req.getLoginName());
+//            LOG
+// 用户名不存在
+
+            throw new BusinessException(BusinessExceptionCode.LOGIN_USER_ERROR);
+        } else {
+            System.out.println("走了嘛");
+            System.out.println(userDB.getPassword().equals(req.getPassword()));
+            if (userDB.getPassword().equals(req.getPassword())) {
+// 登录成功
+                UserLoginResp userLoginResp = CopyUtil.copy(userDB,UserLoginResp.class);
+                return userLoginResp;
+
+            } else {
+// 登录失败
+                throw new BusinessException(BusinessExceptionCode.LOGIN_USER_ERROR);
+
+            }
+        }
     }
 
 
